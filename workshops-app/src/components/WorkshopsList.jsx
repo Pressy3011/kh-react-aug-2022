59f5b627153b130eaa75ca0ad4fdcc928bf67da7
Prashanth Puranik <puranik@digdeeper.in>
2022-08-29 05:05:10 +0530

    conditional rendering

--- workshops-app/src/components/WorkshopsList.jsx
+++ workshops-app/src/components/WorkshopsList.jsx
@@ -1,17 +1,47 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const WorkshopsList = () => {
-    // this function will execute on load
+    // We ask React to create a piece of data for each instance of the component
+    const [ workshops, setWorkshops ] = useState( [] ); // useState returns an array with the value of the variable, followed by a setter function for the variable
+    const [ loading, setLoading ] = useState( true );
+
+    console.log( workshops );
+    console.log( setWorkshops );
+
+    const fetchWorkshops = async () => {
+        const response = await axios.get( `https://workshops-server.herokuapp.com/workshops` );
+        const workshops = response.data;
+        console.log( workshops );
+        setWorkshops( workshops );
+        setLoading( false );
+    };
+
+    // this function will execute on load, and on every refresh
+    // empty array - [], means execute the effecct on component load ONLY
     useEffect(
         () => {
             console.log( 'function passed to useEffect executed' );
-        }
+            fetchWorkshops();
+        },
+        [] /* dependencies array - specify the state, props whose change change should execute the effect */
     );
 
     console.log( 'about to return from rendering function' );
 
     return (
-        <div>We are fetching workshops. Please wait...</div>
+        <div>
+            {
+                loading === true && (
+                    <div>We are fetching workshops. Please wait...</div>
+                )
+            }
+            {
+                loading === false && (
+                    <div>We have fetched {workshops.length} workshops</div>
+                )
+            }
+        </div>
     );
 }
  
