ea06ef173ad4cba8da5d06e69d03a24ac2dd88cd
Prashanth Puranik <puranik@digdeeper.in>
2022-08-29 04:19:25 +0530

    using Link of react-router-dom instead of plain a tag

--- workshops-app/src/components/Menu.jsx
+++ workshops-app/src/components/Menu.jsx
@@ -1,14 +1,15 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Menu = () => {
     return (
         <Navbar bg="light" variant="light">
             <Container>
-                <Navbar.Brand href="/">Workshops App</Navbar.Brand>
+                <Navbar.Brand to="/" as={Link}>Workshops App</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/workshops">List of workshops</Nav.Link>
-                    <Nav.Link href="/feedback">Feedback</Nav.Link>
+                    <Nav.Link to="/" as={Link}>Home</Nav.Link>
+                    <Nav.Link to="/workshops" as={Link}>List of workshops</Nav.Link>
+                    <Nav.Link to="/feedback" as={Link}>Feedback</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
