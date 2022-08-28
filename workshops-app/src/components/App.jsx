e8b985d80976c5798f3beed87a29c2712a44b78d
Prashanth Puranik <puranik@digdeeper.in>
2022-08-28 09:00:49 +0530

    navigation between pages

--- workshops-app/src/components/App.jsx
+++ workshops-app/src/components/App.jsx
@@ -1,14 +1,21 @@
 // sfc
 import { Container } from 'react-bootstrap';
+import { Route } from 'react-router-dom';
 import Menu from './Menu';
 import Home from './Home';
+import WorkshopsList from './WorkshopsList';
 
 const App = () => {
     return (
         <div>
             <Menu />
             <Container className="my-4">
-                <Home />
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/workshops">
+                    <WorkshopsList />
+                </Route>
             </Container>
         </div>
     );
