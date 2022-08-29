bc881977d2e8e1f4b9edfac79fae642b05e04b33
Prashanth Puranik <puranik@digdeeper.in>
2022-08-29 05:45:25 +0530

    using Card

--- workshops-app/src/components/WorkshopsList.jsx
+++ workshops-app/src/components/WorkshopsList.jsx
@@ -1,27 +1,29 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Alert } from 'react-bootstrap';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Alert, Card, Button } from "react-bootstrap";
 
 const WorkshopsList = () => {
     // We ask React to create a piece of data for each instance of the component
-    const [ workshops, setWorkshops ] = useState( [] ); // useState returns an array with the value of the variable, followed by a setter function for the variable
-    const [ loading, setLoading ] = useState( true );
-    const [ error, setError ] = useState( null );
+    const [workshops, setWorkshops] = useState([]); // useState returns an array with the value of the variable, followed by a setter function for the variable
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    console.log( workshops );
-    console.log( setWorkshops );
+    console.log(workshops);
+    console.log(setWorkshops);
 
     const fetchWorkshops = async () => {
         try {
             // axios.get() returns a Promise object
-            const response = await axios.get( `https://workshops-server.herokuapp.com/workshop` );
+            const response = await axios.get(
+                `https://workshops-server.herokuapp.com/workshops`
+            );
             const workshops = response.data;
-            console.log( workshops );
-            setWorkshops( workshops );
-            setLoading( false );
-        } catch( error ) {
-            setError( error );
-            setLoading( false );
+            console.log(workshops);
+            setWorkshops(workshops);
+            setLoading(false);
+        } catch (error) {
+            setError(error);
+            setLoading(false);
         }
     };
 
@@ -29,41 +31,49 @@ const WorkshopsList = () => {
     // empty array - [], means execute the effecct on component load ONLY
     useEffect(
         () => {
-            console.log( 'function passed to useEffect executed' );
+            console.log("function passed to useEffect executed");
             fetchWorkshops();
         },
         [] /* dependencies array - specify the state, props whose change change should execute the effect */
     );
 
-    console.log( 'about to return from rendering function' );
+    console.log("about to return from rendering function");
 
     return (
         <div>
-            {
-                loading === true && (
-                    <Alert variant="info">We are fetching workshops. Please wait...</Alert>
-                )
-            }
-            {
-                loading === false && error !== null && (
-                    <Alert variant="danger">{error.message}</Alert>
-                )
-            }
-            {
-                loading === false && error === null && (
-                    <ol>
-                        {
-                            workshops.map(
-                                ( workshop ) => {
-                                    return <li key={workshop.id}>{workshop.name}</li>
-                                }
-                            )
-                        }
-                    </ol>
-                )
-            }
+            {loading === true && (
+                <Alert variant="info">
+                    We are fetching workshops. Please wait...
+                </Alert>
+            )}
+            {loading === false && error !== null && (
+                <Alert variant="danger">{error.message}</Alert>
+            )}
+            {loading === false && error === null && (
+                <div>
+                    {workshops.map((workshop) => {
+                        return (
+                            <Card style={{ width: "18rem" }} key={workshop.id}>
+                                <Card.Img
+                                    variant="top"
+                                    src={workshop.imageUrl}
+                                />
+                                <Card.Body>
+                                    <Card.Title>{workshop.name}</Card.Title>
+                                    <Card.Text>
+                                        {workshop.startDate} - {workshop.endDate}
+                                    </Card.Text>
+                                    <Button variant="primary">
+                                        Know more
+                                    </Button>
+                                </Card.Body>
+                            </Card>
+                        );
+                    })}
+                </div>
+            )}
         </div>
     );
-}
- 
+};
+
 export default WorkshopsList;
